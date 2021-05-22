14ff3a0b94b5288f13d24d6e315f665bab2648f7
Jerry <jerry@danceclout.com>
2021-05-22 16:55:51 +0800

    try

---
+++ DSA_EmailSearcher/src/main.c
@@ -0,0 +1,21 @@
+#include "api.h"
+#include "utils.h"
+#include <stdio.h>
+
+// The testdata only contains the first 100 mails (mail1 ~ mail100)
+// and 2000 queries for you to debug.
+
+int main(void)
+{
+  int n_mails, n_queries;
+  mail *mails;
+  query *queries;
+
+  api.init(&n_mails, &n_queries, &mails, &queries);
+
+  for (int i = 0; i < n_queries; i++)
+    if (queries[i].type == expression_match)
+      api.answer(queries[i].id, NULL, 0);
+
+  return 0;
+}

3865701a710833f85408623b53b5e86688a6daa2
stevengogogo <stevengogogo4321@gmail.com>
2021-05-25 11:44:27 +0000

    REver interface

--- src/main.c
+++ src/main.c
@@ -1,33 +1,20 @@
 #include "api.h"
 #include "utils.h"
 #include <stdio.h>
-#include "stringParse.h"
 
 // The testdata only contains the first 100 mails (mail1 ~ mail100)
 // and 2000 queries for you to debug.
 
-int main(void)
-{
-      char message[] = "I23 have1 A_ very- good) (dAy+";
-    int n_token = 6;
-    char tokenStr[6][300] ={ 
-        "i23",
-        "have1",
-        "a",
-        "very",
-        "good",
-        "day"
-    };
+int main(void) {
+    int n_mails, n_queries;
+    mail *mails;
+    query *queries;
 
+	api.init(&n_mails, &n_queries, &mails, &queries);
 
-    char token[2048];
-    int iStr = 0;
-
-    for (int i=0;i<n_token;i++){
-        iStr = popToken(message, token, iStr);
-        if(strcmp(token, tokenStr[i]) == 0);
-          printf("Expected: %s ; Got: %s (%d)\n",tokenStr[i], token, iStr);
-    }
+	for(int i = 0; i < n_queries; i++)
+		if(queries[i].type == expression_match)
+		  api.answer(queries[i].id, NULL, 0);
 
   return 0;
 }

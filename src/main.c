f215a662a343099c7c77809bf429fc7c571e07e4
stevengogogo <stevengogogo4321@gmail.com>
2021-06-20 12:13:15 +0000

    recover main

--- src/main.c
+++ src/main.c
@@ -9,21 +9,17 @@
 // The testdata only contains the first 100 mails (mail1 ~ mail100)
 // and 2000 queries for you to debug.
 
-int main(void) {
-        uArray arr;
-    init_uArray(&arr, sizeof(long));
-    int num = 15000;
-    int item;
-    long i;
-    clock_t str;
-    clock_t end;
 
-    str = clock();
-    for(i=0;i<num;i++){
-        append_uArray(&arr, &i); //[1...100000]
-    }
-    end = clock();
-    //print_clock("uArray Append",str, end);
+int main(void) {
+    int n_mails, n_queries;
+    mail *mails;
+    query *queries;
+
+	api.init(&n_mails, &n_queries, &mails, &queries);
+
+	for(int i = 0; i < n_queries; i++)
+		if(queries[i].type == expression_match)
+		  api.answer(queries[i].id, NULL, 0);
 
   return 0;
 }

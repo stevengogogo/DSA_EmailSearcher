ec5fe87ebed685e207b1550ac1bc8ef06de4791a
stevengogogo <stevengogogo4321@gmail.com>
2021-06-21 10:34:30 +0000

    init

--- src/GroupAnalysis.h
+++ src/GroupAnalysis.h
@@ -1,7 +1,19 @@
 #ifndef GROUPANALYSIS_H
 #define GROUPANALYSIS_H
 #include "utils.h"
+#define Q 100000
+/** Token Information*/
+typedef struct TokenInfo{
+    int occur;
+    dymArr* loc;
+} TokenInfo;
 
+/** Text Summary*/
+typedef struct TxtSmry{
+    TokenInfo token[Q];
+    dymArr* nonZero; //Token locations
+    int nToken;
+} TxtSmry;
 
 
 #endif

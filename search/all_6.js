78dbe3f4ff7f28c8c8a1b02c6639880c8a6d5e41
stevengogogo <stevengogogo@users.noreply.github.com>
2021-06-22 10:53:52 +0000

    deploy: d588eaf81a080ac744c83dd0b6722b9a595f6bcf

--- search/all_6.js
+++ search/all_6.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['indomainint_10',['inDomainInt',['../utils_8h.html#a5e5de4bf768b83c8bb0f35e75cce4d84',1,'utils.c']]],
   ['init_5fdymarr_5fulong_11',['init_dymArr_ULONG',['../FindSimilar_8h.html#a0d232081d3f9372b51d3cbf1de0ad4f7',1,'FindSimilar.c']]],
-  ['init_5fmem_5ffindsimilar_12',['Init_MEM_FindSimilar',['../FindSimilar_8h.html#aefd6fa50a25517ace3eda16981f303e4',1,'FindSimilar.c']]],
+  ['init_5ffindsimilar_12',['Init_FindSimilar',['../FindSimilar_8h.html#abcef5cbf6a37e7160503d572705094cc',1,'FindSimilar.c']]],
   ['init_5fmem_5fulong_13',['init_MEM_ULONG',['../FindSimilar_8h.html#a788083b928d53aaefab439f02b2b3133',1,'FindSimilar.c']]],
   ['init_5ftxtsmry_14',['init_TxtSmry',['../FindSimilar_8h.html#a1161ac42a619f6eb9ed7589d299a18f1',1,'FindSimilar.c']]],
   ['init_5ftxtsmry_5farr_15',['init_TxtSmry_arr',['../FindSimilar_8h.html#a0ab7f47d3de757cf44fb6d774c224e21',1,'FindSimilar.c']]],

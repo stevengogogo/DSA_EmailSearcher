a01f0b747b10dca701f2324e1e3fbee3f35f2dd0
stevengogogo <stevengogogo@users.noreply.github.com>
2021-06-26 17:50:31 +0000

    deploy: 25776b2b309d3fc8ef0348f20b10b8e0b242cc87

--- search/classes_1.js
+++ search/classes_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['mail_44',['mail',['../structmail.html',1,'']]],
-  ['matrix_45',['Matrix',['../structMatrix.html',1,'']]],
-  ['memory_5fshort_46',['MEMORY_SHORT',['../structMEMORY__SHORT.html',1,'']]],
-  ['memory_5fulong_47',['MEMORY_ULONG',['../structMEMORY__ULONG.html',1,'']]]
+  ['mail_43',['mail',['../structmail.html',1,'']]],
+  ['matrix_44',['Matrix',['../structMatrix.html',1,'']]],
+  ['memory_5fshort_45',['MEMORY_SHORT',['../structMEMORY__SHORT.html',1,'']]],
+  ['memory_5fulong_46',['MEMORY_ULONG',['../structMEMORY__ULONG.html',1,'']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['kill_5fmem_5fulong_71',['kill_MEM_ULONG',['../FindSimilar_8h.html#a6e2dd2f8a6e68dd5aaca7c76a32ddc05',1,'FindSimilar.c']]],
-  ['kill_5ftxtsmry_5farr_72',['kill_TxtSmry_arr',['../FindSimilar_8h.html#a59b118272255ea41350b6fc60bff4ecd',1,'FindSimilar.c']]]
+  ['kill_5fmem_5fulong_69',['kill_MEM_ULONG',['../FindSimilar_8h.html#a6e2dd2f8a6e68dd5aaca7c76a32ddc05',1,'FindSimilar.c']]],
+  ['kill_5ftxtsmry_5farr_70',['kill_TxtSmry_arr',['../FindSimilar_8h.html#a59b118272255ea41350b6fc60bff4ecd',1,'FindSimilar.c']]]
 ];
